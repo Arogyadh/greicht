93e9b3c50e982f55bac8374edb55a7a47a96993d
unknown <arogyadhdl@gmail.com>
2023-11-01 08:02:50 +0545

    Fixed z-index for IntroAnim

--- src/components/IntroAnim/IntroAnim.jsx
+++ src/components/IntroAnim/IntroAnim.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { images } from "../../constants";
 import gsap from "gsap";
-import "./IntroAnim.css"; // Consider creating a CSS file for styles
+import "./IntroAnim.css";
 
 const IntroAnim = () => {
   const imageRef = useRef(null);
+  const [zIndex, setZIndex] = useState(9999);
 
   useEffect(() => {
     const introAnimation = () => {
@@ -14,14 +15,21 @@ const IntroAnim = () => {
       gsap.fromTo(imageElement, { opacity: 0 }, { opacity: 1, duration: 2 });
       gsap.to(imageElement, { opacity: 0, duration: 2, delay: 1 });
 
-      gsap.to(overlay, { opacity: 0, duration: 2, delay: 1 });
+      gsap.to(overlay, {
+        opacity: 0,
+        duration: 2,
+        delay: 1,
+        onComplete: () => {
+          setZIndex(-100);
+        },
+      });
     };
 
     introAnimation();
   }, []);
 
   return (
-    <div className="intro-overlay">
+    <div className="intro-overlay" style={{ zIndex: zIndex }}>
       <img
         ref={imageRef}
         src={images.G}
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,49 +1,15 @@
 import React, { useEffect, useRef } from "react";
 import { images } from "../../constants";
-import { gsap } from "gsap";
 
 import "./Header.css";
 import { SubHeading } from "../../components";
 
 const Header = () => {
-  const headerRef = React.useRef(null);
-  const headRef = useRef([]);
-
-  useEffect(() => {
-    const headerElement = headerRef.current;
-    const headElements = headRef.current;
-
-    gsap.from(headerElement, {
-      x: -100,
-      opacity: 0,
-      duration: 2,
-      ease: "power2.out",
-    });
-
-    gsap.from(headElements, {
-      duration: 3,
-      opacity: 0,
-      y: -50,
-      ease: "power2.inOut",
-      stagger: 0.2,
-    });
-  }, []);
-
   return (
-    <div
-      ref={headerRef}
-      className="app__header app__wrapper section__padding "
-      id="Home"
-    >
+    <div className="app__header app__wrapper section__padding " id="Home">
       <div className="app__wrapper_info">
         <SubHeading title="Chase the new flavour" />
-        <h1 className="app__header-h1">
-          <span>The</span>
-          <span ref={(el) => (headRef.current[0] = el)}> key</span>
-          <span ref={(el) => (headRef.current[1] = el)}> to</span>
-          <span ref={(el) => (headRef.current[2] = el)}> fine</span>
-          <span ref={(el) => (headRef.current[3] = el)}> Dining</span>
-        </h1>
+        <h1 className="app__header-h1">The key to Fine Dining.</h1>
         <p className="p__opensans" style={{ margin: "2rem 0" }}>
           Explore a perfect fusion of flavors, textures, and passion. Indulge in
           our culinary artistry that crafts unforgettable dining experiences.
