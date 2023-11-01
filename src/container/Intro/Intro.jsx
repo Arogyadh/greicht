b48691bc0b74e4fb2319c6a3edf340832dcaa0db
unknown <arogyadhdl@gmail.com>
2023-11-01 10:04:54 +0545

    Added preload and poster for video

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
-import { meal } from "../../constants";
-
+import { meal, images } from "../../constants";
 import "./Intro.css";
 
 const Intro = () => {
   const [playVideo, setPlayVideo] = useState(false);
-  const vidRef = React.useRef();
+  const vidRef = useRef();
+
   const handleVideo = () => {
     setPlayVideo((prevPlayVideo) => !prevPlayVideo);
 
-    if (playVideo === true) {
-      vidRef.current.pause();
+    if (!playVideo) {
+      vidRef.current.play().catch((error) => {
+        // Handle play error, if required
+        console.error("Video playback error:", error);
+      });
     } else {
-      vidRef.current.play();
+      vidRef.current.pause();
     }
   };
 
@@ -26,6 +29,8 @@ const Intro = () => {
         loop
         controls={false}
         muted
+        preload="auto"
+        poster={images.logo}
       />
       <div className="app__video-overlay flex__center">
         <div
